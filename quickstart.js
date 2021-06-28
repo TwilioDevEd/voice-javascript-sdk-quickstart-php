e65220ccd2c0fdc73cd9ab91baab9b193d59915d
Sarah Stringer <sstringer@twilio.com>
2021-06-28 14:49:40 -0700

    Update static files to use new voice js sdk.
    Also updates the html and css files for clarity/to improve
    layout.

--- quickstart.js
+++ quickstart.js
@@ -1,203 +1,307 @@
 ﻿$(function () {
-  var speakerDevices = document.getElementById('speaker-devices');
-  var ringtoneDevices = document.getElementById('ringtone-devices');
-  var outputVolumeBar = document.getElementById('output-volume');
-  var inputVolumeBar = document.getElementById('input-volume');
-  var volumeIndicators = document.getElementById('volume-indicators');
+  const speakerDevices = document.getElementById("speaker-devices");
+  const ringtoneDevices = document.getElementById("ringtone-devices");
+  const outputVolumeBar = document.getElementById("output-volume");
+  const inputVolumeBar = document.getElementById("input-volume");
+  const volumeIndicators = document.getElementById("volume-indicators");
+  const callButton = document.getElementById("button-call");
+  const hangupButton = document.getElementById("button-hangup");
+  const callControlsDiv = document.getElementById("call-controls");
+  const audioSelectionDiv = document.getElementById("output-selection");
+  const getAudioDevicesButton = document.getElementById("get-devices");
+  const logDiv = document.getElementById("log");
+  const incomingCallDiv = document.getElementById("incoming-call");
+  const incomingCallHangupButton = document.getElementById(
+    "button-hangup-incoming"
+  );
+  const incomingCallAcceptButton = document.getElementById(
+    "button-accept-incoming"
+  );
+  const incomingCallRejectButton = document.getElementById(
+    "button-reject-incoming"
+  );
+  const phoneNumberInput = document.getElementById("phone-number");
+  const incomingPhoneNumberEl = document.getElementById("incoming-number");
+  const startupButton = document.getElementById("startup-button");
 
-  log('Requesting Capability Token...');
-  $.getJSON("/token.php")
-    .then(function (data) {
+  let device;
+  let token;
+
+  // Event Listeners
+
+  callButton.onclick = (e) => {
+    e.preventDefault();
+    makeOutgoingCall();
+  };
+  getAudioDevicesButton.onclick = getAudioDevices;
+  speakerDevices.addEventListener("change", updateOutputDevice);
+  ringtoneDevices.addEventListener("change", updateRingtoneDevice);
+  hangupButton.onclick = hangup;
+
+  // SETUP STEP 1:
+  // Browser client should be started after a user gesture
+  // to avoid errors in the browser console re: AudioContext
+  startupButton.addEventListener("click", startupClient);
+
+  // SETUP STEP 2: Request an Access Token
+  async function startupClient() {
+    log("Requesting Access Token...");
+
+    try {
+      const data = await $.getJSON("/token.php");
       log("Got a token.");
-      console.log("Token: " + data.token);
-
-      // Setup Twilio.Device
-      device = new Twilio.Device(data.token, {
-        // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
-        // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
-        codecPreferences: ["opus", "pcmu"],
-        // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
-        // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
-        // a second time and sending the tone twice. This will be default in 2.0.
-        fakeLocalDTMF: true,
-        // Use `enableRingingState` to enable the device to emit the `ringing`
-        // state. The TwiML backend also needs to have the attribute
-        // `answerOnBridge` also set to true in the `Dial` verb. This option
-        // changes the behavior of the SDK to consider a call `ringing` starting
-        // from the connection to the TwiML backend to when the recipient of
-        // the `Dial` verb answers.
-        enableRingingState: true
-      });
-
-      device.on("ready", function (device) {
-        log("Twilio.Device Ready!");
-        document.getElementById("call-controls").style.display = "block";
-      });
-
-      device.on("error", function (error) {
-        log("Twilio.Device Error: " + error.message);
-      });
-
-      device.on("connect", function (conn) {
-        log("Successfully established call!");
-        document.getElementById("button-call").style.display = "none";
-        document.getElementById("button-hangup").style.display = "inline";
-        volumeIndicators.style.display = "block";
-        bindVolumeIndicators(conn);
-      });
-
-      device.on("disconnect", function (conn) {
-        log("Call ended.");
-        document.getElementById("button-call").style.display = "inline";
-        document.getElementById("button-hangup").style.display = "none";
-        volumeIndicators.style.display = "none";
-      });
-
-      device.on("incoming", function (conn) {
-        log("Incoming connection from " + conn.parameters.From);
-        var archEnemyPhoneNumber = "+12093373517";
-
-        if (conn.parameters.From === archEnemyPhoneNumber) {
-          conn.reject();
-          log("It's your nemesis. Rejected call.");
-        } else {
-          // accept the incoming connection and start two-way audio
-          conn.accept();
-        }
-      });
-
+      token = data.token;
       setClientNameUI(data.identity);
-
-      device.audio.on("deviceChange", updateAllDevices.bind(device));
-
-      // Show audio selection UI if it is supported by the browser.
-      if (device.audio.isOutputSelectionSupported) {
-        document.getElementById("output-selection").style.display = "block";
-      }
-    })
-    .catch(function (err) {
+      intitializeDevice();
+    } catch (err) {
       console.log(err);
-      log("Could not get a token from server!");
+      log("An error occurred. See your browser console for more information.");
+    }
+  }
+
+  // SETUP STEP 3:
+  // Instantiate a new Twilio.Device
+  function intitializeDevice() {
+    logDiv.classList.remove("hide");
+    log("Initializing device");
+    device = new Twilio.Device(token, {
+      debug: true,
+      answerOnBridge: true,
+      // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
+      // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
+      codecPreferences: ["opus", "pcmu"],
     });
 
-  // Bind button to make call
-  document.getElementById("button-call").onclick = function () {
-    // get the phone number to connect the call to
+    addDeviceListeners(device);
+
+    // Device must be registered in order to receive incoming calls
+    device.register();
+  }
+
+  // SETUP STEP 4:
+  // Listen for Twilio.Device states
+  function addDeviceListeners(device) {
+    device.on("registered", function () {
+      log("Twilio.Device Ready to make and receive calls!");
+      callControlsDiv.classList.remove("hide");
+    });
+
+    device.on("error", function (error) {
+      log("Twilio.Device Error: " + error.message);
+    });
+
+    device.on("incoming", handleIncomingCall);
+
+    device.audio.on("deviceChange", updateAllAudioDevices.bind(device));
+
+    // Show audio selection UI if it is supported by the browser.
+    if (device.audio.isOutputSelectionSupported) {
+      audioSelectionDiv.classList.remove("hide");
+    }
+  }
+
+  // MAKE AN OUTGOING CALL
+
+  async function makeOutgoingCall() {
     var params = {
-      To: document.getElementById("phone-number").value
+      // get the phone number to call from the DOM
+      phone: phoneNumberInput.value,
     };
 
-    console.log("Calling " + params.To + "...");
     if (device) {
-      var outgoingConnection = device.connect(params);
-      outgoingConnection.on("ringing", function () {
-        log("Ringing...");
-      });
-    }
-  };
+      log(`Attempting to call ${params.phone} ...`);
 
-  // Bind button to hangup call
-  document.getElementById("button-hangup").onclick = function () {
-    log("Hanging up...");
+      // Twilio.Device.connect() returns a Call object
+      const call = await device.connect({ params });
+
+      // add listeners to the Call
+      // "accepted" means the call has finished connecting and the state is now "open"
+      call.addListener("accept", updateUIAcceptedCall);
+      call.addListener("disconnect", updateUIDisconnectedCall);
+    } else {
+      log("Unable to make call.");
+    }
+  }
+
+  function updateUIAcceptedCall(call) {
+    log("Call in progress ...");
+    callButton.disabled = true;
+    hangupButton.classList.remove("hide");
+    volumeIndicators.classList.remove("hide");
+    bindVolumeIndicators(call);
+  }
+
+  function updateUIDisconnectedCall() {
+    log("Call disconnected.");
+    callButton.disabled = false;
+    hangupButton.classList.add("hide");
+    volumeIndicators.classList.add("hide");
+  }
+
+  // HANG UP A CALL
+
+  function hangup() {
+    log("Hanging up ...");
     if (device) {
       device.disconnectAll();
     }
-  };
+  }
 
-  document.getElementById("get-devices").onclick = function () {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then(updateAllDevices.bind(device));
-  };
+  // HANDLE INCOMING CALL
 
-  speakerDevices.addEventListener("change", function () {
-    var selectedDevices = [].slice
-      .call(speakerDevices.children)
-      .filter(function (node) {
-        return node.selected;
-      })
-      .map(function (node) {
-        return node.getAttribute("data-id");
-      });
+  function handleIncomingCall(call) {
+    log(`Incoming call from ${call.parameters.From}`);
+
+    //show incoming call div and incoming phone number
+    incomingCallDiv.classList.remove("hide");
+    incomingPhoneNumberEl.innerHTML = call.parameters.From;
+
+    //add event listeners for Accept, Reject, and Hangup buttons
+    incomingCallAcceptButton.onclick = () => {
+      acceptIncomingCall(call);
+    };
+
+    incomingCallRejectButton.onclick = () => {
+      rejectIncomingCall(call);
+    };
+
+    incomingCallHangupButton.onclick = () => {
+      hangupIncomingCall(call);
+    };
+
+    // add event listener to call object
+    call.addListener("cancel", handleDisconnectedIncomingCall);
+  }
+
+  // ACCEPT INCOMING CALL
+
+  function acceptIncomingCall(call) {
+    call.accept();
+
+    //update UI
+    log("Accepted incoming call.");
+    incomingCallAcceptButton.classList.add("hide");
+    incomingCallRejectButton.classList.add("hide");
+    incomingCallHangupButton.classList.remove("hide");
+  }
+
+  // REJECT INCOMING CALL
+
+  function rejectIncomingCall(call) {
+    call.reject();
+
+    log("Rejected incoming call");
+    resetIncomingCallUI();
+  }
+
+  // HANG UP INCOMING CALL
+
+  function hangupIncomingCall() {
+    hangup();
+
+    resetIncomingCallUI();
+  }
+
+  // HANDLE CANCELLED INCOMING CALL
+
+  function handleDisconnectedIncomingCall(call) {
+    device.disconnectAll();
+    console.log("handledisconnected : ", call);
+    log("Incoming call ended.");
+    resetIncomingCallUI();
+  }
+
+  // MISC USER INTERFACE
+
+  // Activity log
+  function log(message) {
+    logDiv.innerHTML += `<p class="log-entry">&gt;&nbsp; ${message} </p>`;
+    logDiv.scrollTop = logDiv.scrollHeight;
+  }
+
+  function setClientNameUI(clientName) {
+    var div = document.getElementById("client-name");
+    div.innerHTML = `Your client name: <strong>${clientName}</strong>`;
+  }
+
+  function resetIncomingCallUI() {
+    incomingPhoneNumberEl.innerHTML = "";
+    incomingCallDiv.classList.add("hide");
+  }
+
+  // AUDIO CONTROLS
+
+  async function getAudioDevices() {
+    await navigator.mediaDevices.getUserMedia({ audio: true });
+    updateAllAudioDevices.bind(device);
+  }
+
+  function updateAllAudioDevices() {
+    if (device) {
+      updateDevices(speakerDevices, device.audio.speakerDevices.get());
+      updateDevices(ringtoneDevices, device.audio.ringtoneDevices.get());
+    }
+  }
+
+  function updateOutputDevice() {
+    const selectedDevices = Array.from(speakerDevices.children)
+      .filter((node) => node.selected)
+      .map((node) => node.getAttribute("data-id"));
 
     device.audio.speakerDevices.set(selectedDevices);
-  });
+  }
 
-  ringtoneDevices.addEventListener("change", function () {
-    var selectedDevices = [].slice
-      .call(ringtoneDevices.children)
-      .filter(function (node) {
-        return node.selected;
-      })
-      .map(function (node) {
-        return node.getAttribute("data-id");
-      });
+  function updateRingtoneDevice() {
+    const selectedDevices = Array.from(ringtoneDevices.children)
+      .filter((node) => node.selected)
+      .map((node) => node.getAttribute("data-id"));
 
     device.audio.ringtoneDevices.set(selectedDevices);
-  });
+  }
 
-  function bindVolumeIndicators(connection) {
-    connection.volume(function (inputVolume, outputVolume) {
-      var inputColor = 'red';
-      if (inputVolume < .50) {
-        inputColor = 'green';
-      } else if (inputVolume < .75) {
-        inputColor = 'yellow';
+  function bindVolumeIndicators(call) {
+    call.on("volume", function (inputVolume, outputVolume) {
+      var inputColor = "red";
+      if (inputVolume < 0.5) {
+        inputColor = "green";
+      } else if (inputVolume < 0.75) {
+        inputColor = "yellow";
       }
 
-      inputVolumeBar.style.width = Math.floor(inputVolume * 300) + 'px';
+      inputVolumeBar.style.width = Math.floor(inputVolume * 300) + "px";
       inputVolumeBar.style.background = inputColor;
 
-      var outputColor = 'red';
-      if (outputVolume < .50) {
-        outputColor = 'green';
-      } else if (outputVolume < .75) {
-        outputColor = 'yellow';
+      var outputColor = "red";
+      if (outputVolume < 0.5) {
+        outputColor = "green";
+      } else if (outputVolume < 0.75) {
+        outputColor = "yellow";
       }
 
-      outputVolumeBar.style.width = Math.floor(outputVolume * 300) + 'px';
+      outputVolumeBar.style.width = Math.floor(outputVolume * 300) + "px";
       outputVolumeBar.style.background = outputColor;
     });
   }
 
-  function updateAllDevices() {
-    updateDevices(speakerDevices, device.audio.speakerDevices.get());
-    updateDevices(ringtoneDevices, device.audio.ringtoneDevices.get());
+  // Update the available ringtone and speaker devices
+  function updateDevices(selectEl, selectedDevices) {
+    selectEl.innerHTML = "";
+
+    device.audio.availableOutputDevices.forEach(function (device, id) {
+      var isActive = selectedDevices.size === 0 && id === "default";
+      selectedDevices.forEach(function (device) {
+        if (device.deviceId === id) {
+          isActive = true;
+        }
+      });
+
+      var option = document.createElement("option");
+      option.label = device.label;
+      option.setAttribute("data-id", id);
+      if (isActive) {
+        option.setAttribute("selected", "selected");
+      }
+      selectEl.appendChild(option);
+    });
   }
 });
-
-// Update the available ringtone and speaker devices
-function updateDevices(selectEl, selectedDevices) {
-  selectEl.innerHTML = "";
-
-  device.audio.availableOutputDevices.forEach(function (device, id) {
-    var isActive = selectedDevices.size === 0 && id === "default";
-    selectedDevices.forEach(function (device) {
-      if (device.deviceId === id) {
-        isActive = true;
-      }
-    });
-
-    var option = document.createElement("option");
-    option.label = device.label;
-    option.setAttribute("data-id", id);
-    if (isActive) {
-      option.setAttribute("selected", "selected");
-    }
-    selectEl.appendChild(option);
-  });
-}
-
-// Activity log
-function log(message) {
-  var logDiv = document.getElementById('log');
-  logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
-  logDiv.scrollTop = logDiv.scrollHeight;
-}
-
-// Set the client name in the UI
-function setClientNameUI(clientName) {
-  var div = document.getElementById('client-name');
-  div.innerHTML = 'Your client name: <strong>' + clientName +
-    '</strong>';
-}
